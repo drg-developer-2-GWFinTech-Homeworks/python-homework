0039cec26bc7f45433daf84ba27f3a9a867a1057
David G <davidrgayman@gmail.com>
2020-10-02 16:15:28 -0400

    Saving progress.

---
+++ PyBank/pyBankFunctions.py
@@ -0,0 +1,10 @@
+def generateReport(numMonths, profit_total, averageMonthlyProfit):
+    return [
+        "Financial Analysis",
+        "----------------------------",
+        "Total Months: %i" % numMonths,
+        "Total: $%.2f" % profit_total,
+        "Average  Change: $%.2f" % averageMonthlyProfit,
+        "Greatest Increase in Profits: %s ($%.2f)" % (maxMonthlyProfitabilityMonth, maxMonthlyProfitability),
+        "Greatest Decrease in Profits: %s ($%.2f)"% (minMonthlyProfitabilityMonth, minMonthlyProfitability)
+    ]

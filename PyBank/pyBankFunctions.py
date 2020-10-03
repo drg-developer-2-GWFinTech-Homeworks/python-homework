76d13a29cf46793bc92a4031643de556fdeeffc7
David G <davidrgayman@gmail.com>
2020-10-03 17:46:31 -0400

    Fixing up.

--- PyBank/pyBankFunctions.py
+++ PyBank/pyBankFunctions.py
@@ -1,12 +1,15 @@
-def generateReport():
+def generateReport2():
     return "inside gr"
-def generateReport(numMonths, profit_total, averageMonthlyProfit):
+
+def generateReport(number_of_months, total_profit, average_change_in_monthly_profit,
+                  best_monthly_change_month, best_monthly_change_value,
+                  worst_monthly_change_month, worst_monthly_change_value):
     return [
         "Financial Analysis",
         "----------------------------",
-        "Total Months: %i" % numMonths,
-        "Total: $%.2f" % profit_total,
-        "Average  Change: $%.2f" % averageMonthlyProfit,
-        "Greatest Increase in Profits: %s ($%.2f)" % (maxMonthlyProfitabilityMonth, maxMonthlyProfitability),
-        "Greatest Decrease in Profits: %s ($%.2f)"% (minMonthlyProfitabilityMonth, minMonthlyProfitability)
+        "Total Months: %i" % number_of_months,
+        "Total: $%.2f" % total_profit,
+        "Average  Change: $%.2f" % average_change_in_monthly_profit,
+        "Greatest Increase in Profits: %s ($%.2f)" % (best_monthly_change_month, best_monthly_change_value),
+        "Greatest Decrease in Profits: %s ($%.2f)"% (worst_monthly_change_month, worst_monthly_change_value)
     ]
